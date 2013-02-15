cc524b4ac9a21553be13f338ce1b2db0e89151fa
Kevin Scannell <kscanne@gmail.com>
2013-02-15 07:11:06 -0600

    change lang2po so that it removes msgstr=msgid translations

--- scripts/lang2po.sh
+++ scripts/lang2po.sh
@@ -5,6 +5,8 @@ then
 	exit 1
 fi
 TEMPFILE=`mktemp`
+TEMPFILE2=`mktemp`
 cat "${1}" | iconv -f utf16 -t utf8 | tr -d "\015" | sed 's/["\\]/\\&/g' | sed 's/=/\t/' | LC_ALL=C sort -k1,1 > $TEMPFILE
-(cat ../po/header.pot; cat ../lang/English.lang | iconv -f utf16 -t utf8 | tr -d "\015" | sed 's/["\\]/\\&/g' | sed 's/=/\t/' | LC_ALL=C sort -k1,1 | LC_ALL=C join -t $'\t' - $TEMPFILE | tr "\t" '~' | sed 's/^\([^~]*\)~\([^~]*\)~\(.*\)$/msgctxt "\1"\nmsgid "\2"\nmsgstr "\3"\n/')
-rm -f $TEMPFILE
+(cat ../po/header.pot; cat ../lang/English.lang | iconv -f utf16 -t utf8 | tr -d "\015" | sed 's/["\\]/\\&/g' | sed 's/=/\t/' | LC_ALL=C sort -k1,1 | LC_ALL=C join -t $'\t' - $TEMPFILE | tr "\t" '~' | egrep -v '^[^~]+~([^~]+)~\1$' | sed 's/^\([^~]*\)~\([^~]*\)~\(.*\)$/msgctxt "\1"\nmsgid "\2"\nmsgstr "\3"\n/') > $TEMPFILE2
+msgmerge $TEMPFILE2 ../po/skype.pot 2> /dev/null | sed '3d'
+rm -f $TEMPFILE $TEMPFILE2
